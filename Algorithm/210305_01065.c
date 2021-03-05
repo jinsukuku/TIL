0fc51dc6f9dc7aa971318f4634a89c833b7d2772
dev-ku <jinsukuku@gmail.com>
2021-03-05 22:46:10 +0900

    21.03.05
    백준 01065번 / C언어
    (+) 함수 형태로 수정

--- Algorithm/210305_01065.c
+++ Algorithm/210305_01065.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+int find(int);
 int main(){
     //문제01065 / 2021.03.05
     //1부터 입력한 숫자까지의 숫자 중
     //모든 자릿수가 등차수열을 이루는 경우를 카운트해 출력
-    int n, cnt=0;
-    int n1,n2;
+    int n;
     scanf("%d",&n);
+    printf("%d\n",find(n));
+    return 0;
+}
+
+int find(int n){
+    int n1,n2,cnt=0;
     for(int i=1; i<n+1; i++){
         if(i<100) {
             cnt++;
@@ -19,6 +25,5 @@ int main(){
             }
         }
     }
-    printf("%d\n",cnt);
-    return 0;
+    return cnt;
 }
