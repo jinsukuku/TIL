7a041b813d3ab7c78c9f44b7471d55b05fd0eff7
dev-ku <jinsukuku@gmail.com>
2021-09-28 20:04:52 +0900

    Test: 02.Variables, DataType

--- JavaScript/예제/02-1.hosting.js
+++ JavaScript/예제/02-1.hosting.js
@@ -5,14 +5,13 @@ console.log(nameA);         // undefined
 var nameA = "jjangu";
 console.log(nameA);         // jjangu
 
-
-// caseA의 실제 동작 순서 (호이스팅이 발생)
-var nameA;
+// caseB의 실제 동작 순서 (호이스팅이 발생)
+var nameB;
 console.log(nameA);         // undefined
 nameB = "jjangu";
 console.log(nameA);         // jjangu
 
-// caseB : let을 사용한 경우
-console.log(nameB);         // ERROR : 선언되어있지 않은 변수이므로 찾을 수 없기때문에 오류 발생
-let nameB = "jjangu";
-console.log(nameB);         // jjangu
+// caseC : let을 사용한 경우
+console.log(nameC);         // ERROR : 선언되어있지 않은 변수이므로 찾을 수 없기때문에 오류 발생
+let nameC = "jjangu";
+console.log(nameC);         // jjangu
