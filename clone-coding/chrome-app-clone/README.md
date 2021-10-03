804ba93cf0cecdbe18b52bc97b552db8a6b800d4
dev-ku <jinsukuku@gmail.com>
2021-10-03 17:55:51 +0900

    Add: Clone-coding result

--- clone-coding/chrome-app-clone/README.md
+++ clone-coding/chrome-app-clone/README.md
@@ -1,4 +1,6 @@
 # 📌 Chrome-app-Clone
+![clone_result](/필기/img/clone_result.gif)
+
 - 바닐라 JS로 크롬 앱 모멘텀(Momentum) 클론 코딩하기
 - 모멘텀(Momentum) : https://bit.ly/3o3kVFh
 - 노마드코더 강의 : https://nomadcoders.co/javascript-for-beginners
--- clone-coding/chrome-app-clone/momentum/js/greetings.js
+++ clone-coding/chrome-app-clone/momentum/js/greetings.js
@@ -6,15 +6,6 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hiddenClass";
 const USERNAME_KEY = "username";
 
-// <input> 태그의 기본 속성으로 손쉽게 해결 가능 
-// function onLoginBtnclick(){
-    // if (val == ""){
-    //     console.log("input your name");
-    // } else if(val.length > 15){
-    //     alert("too long");
-    // }
-// };
-
 function paintGreetings(username){
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerText= `Hello, ${username}`;
