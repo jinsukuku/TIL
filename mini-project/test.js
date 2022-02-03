0b0e34655730e91076c4bf6519612b407ae94bce
jschoi <jinsukuku@gmail.com>
2022-02-03 13:03:47 +0900

    Docs: update 2-1.Array-like Object

--- mini-project/test.js
+++ mini-project/test.js
@@ -1,3 +1,7 @@
+let users = ["Mike", "Tom", "Jane"];
+let [user1, user2, user3] = users;
 
-console.log( Array.isArray( [] ) );
-console.log( Array.isArray( {} ) );
+console.log(user1); // Mike
+console.log(user2); // Tom
+console.log(user3); // Jane
+console.log(users);
