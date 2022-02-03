503214b74f8c4c6cd2e471fb3529420df014f452
jschoi <jinsukuku@gmail.com>
2022-02-03 15:55:14 +0900

    Rename: 01.Array.from()

--- mini-project/test.js
+++ mini-project/test.js
@@ -1,34 +1,14 @@
-const arrlike = {
-    // Symbol.iterator() 
-    [Symbol.iterator]() {
-        this.i = 0;
-        return this;
-    },
 
-    // next()
-    next() {
-        return { value: this[this.i], done: ++this.i > this.length };
-    },
-
-    // index
-    0: "Nice",
-    1: "to",
-    2: "meet",
-    3: "you",
-
-    // length
-    length: 4,
+function Student() {
+    this.name = "짱구";
+    return {
+        name: "유리",
+        // 일반 함수의 this : 생성자 함수의 this이므로, 객체 자기 자신을 의미
+        print: function(){
+            console.log(this.name + "입니다");
+        } 
+    }
 }
 
-for(var txt of arrlike){
-    console.log(txt);
-}
-
-console.log(...arrlike);
-
-var [ a, b, c, d ] = arrlike;
-
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+const student = new Student();
+student.print();    // 유리입니다
