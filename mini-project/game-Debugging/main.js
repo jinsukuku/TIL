12caf7153e4d696bdf52160a3aaa29356f012838
dev-ku <jinsukuku@gmail.com>
2022-02-03 23:17:30 +0900

    Docs: 09. JavaScript_import,require

---
+++ mini-project/game-Debugging/main.js
@@ -0,0 +1,5 @@
+// 📁 main.js
+import {sayHi} from './sayHi.js';
+
+console.log(sayHi); // 함수
+sayHi("하이");
---
+++ mini-project/game-Debugging/sayHi.js
@@ -0,0 +1,12 @@
+// 📁 sayHi.js
+var test = 0;
+
+export function sayHi(user) {
+  console.log(`Hi, ${user}!`);
+  console.log(test);
+}
+
+export function sayHello(user) {
+  console.log(`Hello, ${user}!`);
+}
+
