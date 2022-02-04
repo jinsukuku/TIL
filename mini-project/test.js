c6262f9c3df1c53b7a5eb73aaef61e6e9464eb07
jschoi <jinsukuku@gmail.com>
2022-02-04 09:00:18 +0900

    Docs: ES6 shortcut

--- mini-project/test.js
+++ mini-project/test.js
@@ -1,9 +1,7 @@
-document.body.innerHTML += '<button>버튼1</button><button>버튼2</button>';
-const btnList = document.querySelectorAll('button');
-const [btn1, btn2] = [btnList[0], btnList[1]];
+var same = "test";
+function same() { console.log("함수가 실행될까?") };
 
-// btn1 클릭시 btn1 객체 출력 (this === btn1)
-btn1.addEventListener("click", function() { console.log(this); });
+console.log(typeof same); // String
+same(); // TypeError: same is not a function
 
-// btn2 클릭시 Window 객체 출력 (this === Window)
-btn2.addEventListener("click", () => { console.log(this); });
+
