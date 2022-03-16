a340c30b4c5826e3755e5e02f2e8cfce43a460bd
dev-ku <jinsukuku@gmail.com>
2022-03-16 23:43:57 +0900

    Add: 0.INTRODUCTION(2)

--- clone-coding/zoom-clone/zoom/src/server.js
+++ clone-coding/zoom-clone/zoom/src/server.js
@@ -1,6 +1,19 @@
 import express from "express"
 
 const app = express(); // create app
-app.listen(3000); // port settings
+
+// pug setting 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+// pug에 추가한 스크립트의 static 작업
+app.use("/public", express.static(__dirname + "/public"));
+
+// route
+app.get("/", (req,res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/"));
+
+// port settings
+app.listen(3000); 
 
 console.log("HELLO NODE-WORLD");
