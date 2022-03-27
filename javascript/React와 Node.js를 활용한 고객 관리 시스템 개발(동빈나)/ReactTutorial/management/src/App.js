3e6b2c3447747b742372c421e64bc6562a3ba26f
dev-ku <jinsukuku@gmail.com>
2022-03-27 21:02:21 +0900

    Test: StructuringComponent

--- javascript/React와 Node.js를 활용한 고객 관리 시스템 개발(동빈나)/ReactTutorial/management/src/App.js
+++ javascript/React와 Node.js를 활용한 고객 관리 시스템 개발(동빈나)/ReactTutorial/management/src/App.js
@@ -2,23 +2,50 @@ import './App.css'; // css파일 연결
 import { Component } from 'react';  // Component을 사용하기위헤 import
 import Customer from './components/Customer'; // 만들어둔 Customer 컴포넌트 사용을 위한 import 
 
-const customer = {
-    'name': '한유리',
-    'age': 5,
-    'address': '떡잎마을',
-    'class': '해바라기반',
-}
-
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '짱구',
+  'age': 5,
+  'address': '떡잎마을',
+  'class': '해바라기반',
+},{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '유리',
+  'age': 5,
+  'address': '떡잎마을',
+  'class': '해바라기반',
+},{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '치타',
+  'age': 7,
+  'address': '새싹마을',
+  'class': '장미반',
+},
+]
 class App extends Component {
   render(){
     return (
-      <Customer
-        name = {customer.name}
-        age = {customer.age}
-        address = {customer.address}
-        class = {customer.class}
-      />
-    );
+      <div>
+        {customers.map( c => {
+            return (
+              <Customer
+                key = {c.id}
+                id = {c.id}
+                image = {c.image}
+                name = {c.name}
+                age = {c.age}
+                address = {c.address}
+                class = {c.class}
+              />
+            )
+          })
+        }
+      </div>
+    )
   }
 }
+
 export default App; // App export
--- javascript/React와 Node.js를 활용한 고객 관리 시스템 개발(동빈나)/ReactTutorial/management/src/components/Customer.js
+++ javascript/React와 Node.js를 활용한 고객 관리 시스템 개발(동빈나)/ReactTutorial/management/src/components/Customer.js
@@ -5,12 +5,33 @@ class Customer extends React.Component{
         return(
             // component를 실제 화면에 그릴 때 반드시 필요하다
             <div>
-                <h2>{this.props.name}</h2>
+                <CustomerProfile name = {this.props.name} image = {this.props.image} id = {this.props.id} />
+                <CustomerInfo age = {this.props.age} class = {this.props.class} address = {this.props.address} />
+            </div>
+        );
+    }
+}
+
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component{
+    render(){
+        return(
+            <div>
                 <p>{this.props.age}살</p>
                 <p>{this.props.address}</p>
                 <p>{this.props.class}</p>
             </div>
-        );
+        )
     }
 }
 
