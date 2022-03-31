f6dc119cd8669bf00769f4926a877292376275a3
dev-ku <jinsukuku@gmail.com>
2022-04-01 01:58:46 +0900

    Test: Progress Bar UI

--- javascript/React와 Node.js를 활용한 고객 관리 시스템 개발(동빈나)/ReactTutorial/management/client/src/App.js
+++ javascript/React와 Node.js를 활용한 고객 관리 시스템 개발(동빈나)/ReactTutorial/management/client/src/App.js
@@ -9,6 +9,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@mui/material/CircularProgress';
 
 // css 
 import { withStyles } from '@material-ui/core/styles';
@@ -21,16 +22,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080,
-  }
+  },
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
 })
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0,
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -40,6 +46,11 @@ class App extends Component {
     const response = await fetch('/api/customers'); // api 연결
     const body = await response.json(); // api 로부터 받아온 데이터를 json형태로 
     return body; // return 
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState( { completed: completed >= 100 ? 0 : completed+ 1 });
   }
 
   render(){
@@ -70,7 +81,13 @@ class App extends Component {
                     class = {c.class}
                   />
                 )
-              }) : ""
+              }) :
+              // Progress Bar UI 적용
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className = {classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
