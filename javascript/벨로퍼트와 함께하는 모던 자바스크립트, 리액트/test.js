3e12e2d95dcf3f0a8df04a2c52119154be324857
dev-ku <jinsukuku@gmail.com>
2022-04-05 20:59:29 +0900

    Docs: update 모던 자바스크립트-1장 자바스크립트 입문

--- javascript/벨로퍼트와 함께하는 모던 자바스크립트, 리액트/test.js
+++ javascript/벨로퍼트와 함께하는 모던 자바스크립트, 리액트/test.js
@@ -1,10 +1,19 @@
-
-const student = {
-    name: 'jjangu',
-    age: 5,
-    className: '해바라기반'
-};
-
-for (let key in student) {
-  console.log(`${key}: ${student[key]}`);
+class Animal {
+  constructor(type, name, sound){
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+  }
+  say() {
+    console.log(this.sound);
+  }
 }
+
+class Dog extends Animal{
+  constructor(name, sound){
+    super('Dog', name, sound);
+  }
+}
+
+const dog = new Dog('멍멍이', '멍멍');
+dog.say();
