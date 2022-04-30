609091d809815807391d27b4fd8df9cf8851c04e
dev-ku <jinsukuku@gmail.com>
2022-04-30 10:16:33 +0900

    Add: Boj1463, Boj11576, Boj11653

--- Algorithm/BOJ/Boj11653.py
+++ Algorithm/BOJ/Boj11653.py
@@ -1,11 +1,10 @@
-## 백준 11653. 소인수분해
-## 2022.01.27(목)
+# 11653. 소인수분해
 
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline().rstrip())
+n = int(input().rstrip())
 i = 2
-
 while n != 1:
     if n % i == 0:
         while n % i == 0:
