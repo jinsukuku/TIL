57050af7e0db7de44be2fce76b5d20937c123dc9
dev-ku <jinsukuku@gmail.com>
2022-05-02 23:02:23 +0900

    Docs: 모던 리액트-1.1.0.React

---
+++ javascript/벨로퍼트와 함께하는 모던 자바스크립트, 리액트/ReactProject/begin-react/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Hello from "./Hello";
+import './App.css';
+
+function App() {
+  const name = "React";
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, 
+    padding: '1rem',
+    textAlign: 'center'
+  }
+
+
+  return (
+    <div className="App">
+      <Hello />
+      <h2 style={style}>{name}</h2>
+      <div className="gray-box">GoGo</div>
+    </div>
+  );
+}
+
+export default App;
