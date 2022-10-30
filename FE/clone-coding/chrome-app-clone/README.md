c059ac56fae40ebce917fd998e821aecee66105e
dev-ku <jinsukuku@gmail.com>
2022-10-30 15:08:05 +0900

    Rename: JavaScript

--- FE/clone-coding/chrome-app-clone/README.md
+++
@@ -1,8 +0,0 @@
-# 📌 Chrome-app-Clone
-
-![clone_coding_result](필기/img/clone_result.gif)
-
-- 바닐라 JS로 크롬 앱 모멘텀(Momentum) 클론 코딩하기
-- 모멘텀(Momentum) : https://bit.ly/3o3kVFh
-- 노마드코더 강의 : https://nomadcoders.co/javascript-for-beginners
-- 클론 코딩 결과 : https://dev-ku.github.io/Nmd_chrome_app_clone/
--- FE/clone-coding/chrome-app-clone/강의 코드/js/background.js
+++
@@ -1,7 +0,0 @@
-const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
-
-const chosenImg = images[Math.floor(Math.random() * images.length)];
-
-const bgImg = document.createElement("img");
-bgImg.src = `img/${chosenImg}`;
-document.body.appendChild(bgImg);
